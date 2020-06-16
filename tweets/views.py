2e19d1e374556e081b9b18f874270f0b71f61636
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2020-06-16 10:19:54 +0700

    save with ajax

--- tweets/views.py
+++ tweets/views.py
@@ -8,6 +8,7 @@ from .forms import TweetForm
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
@@ -19,6 +20,8 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm
