84a8ee28599c37bc94299fd7f1c575e6056ae4b2
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2020-06-16 15:24:36 +0700

    superuser

--- tweets/models.py
+++ tweets/models.py
@@ -1,14 +1,21 @@
 import random
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    def __str__(self):
+        return self.content
+
     class Meta:
-        ordering =['-id']
+        ordering = ['-id']
 
     def serialize(self):
         return {
